987ec7c8dddd47fad2d56d2832d751b478e8f021
Viktor Ovcharov <ovcharov.2v@gmail.com>
2023-08-21 22:48:25 +0400

    feat: add Tariffs page

---
+++ src/js/components/_page-tariffs.js
@@ -0,0 +1,11 @@
+import Swiper from "swiper"
+document.addEventListener('DOMContentLoaded', () => {
+	const photoSlider = document.querySelector('.page-tariffs__slider')
+	if(!photoSlider) return
+
+	new Swiper(photoSlider, {
+		slidesPerView: 2,		centeredSlides: true,
+		initialSlide: 1,
+		slideActiveClass: 'page-tariffs__slide-box--active'
+	});
+})
