a0248393ab9587f18b250d4d87ea9cdc43906589
Viktor Ovcharov <ovcharov.2v@gmail.com>
2023-08-23 00:52:52 +0400

    feat: add swipe grid

--- src/js/components/_page-notification.js
+++ src/js/components/_page-notification.js
@@ -1,28 +1,32 @@
 import Hammer from 'hammerjs'
 
 document.addEventListener('DOMContentLoaded', () => {
-	const container = document.querySelector('.page-notification__swipe-container')
-	if(!container) return
-	const manager = new Hammer.Manager(container)
-	const Swipe = new Hammer.Swipe()
-	manager.add(Swipe)
+	const containerList = document.querySelectorAll('.page-notification__swipe-container')
+	if(!containerList.length) return
 
-	manager.on('swipe', function (e) {
-		const direction = e.offsetDirection
-		const elems = container.querySelectorAll('.page-notification__swipe-elem:not(.page-notification__swipe-elem--like):not(.page-notification__swipe-elem--dislike)')
-		if (elems.length) {
-			const el = elems[elems.length - 1]
-			if (direction === 8) {
-				el.classList.add('page-notification__swipe-elem--like')
-				setTimeout(() => {
-					el.remove()
-				}, 400)
-			} else if (direction === 16) {
-				el.classList.add('page-notification__swipe-elem--dislike')
-				setTimeout(() => {
-					el.remove()
-				}, 400)
+	containerList.forEach((container)=>{
+		const manager = new Hammer.Manager(container)
+		const Swipe = new Hammer.Swipe()
+		manager.add(Swipe)
+
+		manager.on('swipe', function (e) {
+			const direction = e.offsetDirection
+			const elems = container.querySelectorAll('.page-notification__swipe-elem:not(.page-notification__swipe-elem--like):not(.page-notification__swipe-elem--dislike)')
+			if (elems.length) {
+				const el = elems[elems.length - 1]
+				if (direction === 4) {
+					el.classList.add('page-notification__swipe-elem--like')
+					setTimeout(() => {
+						el.classList.remove('page-notification__swipe-elem--like')
+					}, 400)
+				} else if (direction === 2) {
+					el.classList.add('page-notification__swipe-elem--dislike')
+					setTimeout(() => {
+						el.classList.remove('page-notification__swipe-elem--dislike')
+					}, 400)
+				}
 			}
-		}
-	});
+		});
+	})
+
 })
